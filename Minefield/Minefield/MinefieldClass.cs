0bdf24aba2fe8e1e9cab5a6b66dcdae9ac927ce0
dalyakob <dalyakob@umich.edu>
2020-05-23 20:49:16 -0400

    first branch

--- Minefield/Minefield/MinefieldClass.cs
+++ Minefield/Minefield/MinefieldClass.cs
@@ -5,6 +5,7 @@ namespace Minefield
     {
         public MinefieldClass()
         {
+
         }
 
         // Declaring public variables
@@ -13,24 +14,31 @@ namespace Minefield
         public int Columns { get; set; }
         public int Bombs { get; set; }
 
-        //hidden board has all the mines and values of adjacent tiles in it 
-        private char[,] _hiddenBoard;
+        private char[,] _hiddenBoard; //hidden board has all the mines and values of adjacent tiles in it 
 
         public char[,] GenerateMinefield()
         {
             Board = new char[Rows, Columns];
-
-            //setting all values of board = ? which are unknown tiles
+            _hiddenBoard = new char[Rows, Columns];
+            //setting all values of board = ? which are unknown cells
             for (int i = 0; i < Rows; i++)
             {
                 for (int j = 0; j < Columns; j++)
                 {
-                    Board[i, j] = '?';
+                    Board[i, j] = '?';// '!' = flag, 'x' = bomb, ' ' = empty, '1','2','3','4' = adjacent blocks
                 }
             }
 
             //generate bombs to hidden board here
             var random = new Random();
+
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Columns; j++)
+                {
+                    _hiddenBoard[i, j] = ' ';
+                }
+            }
 
             return Board;
         }
